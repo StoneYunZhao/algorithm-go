a64947ec23c4eec34282c83b9e30e1db1a233e40
zhaoyun <zhaoyun2316@gmail.com>
2020-07-02 13:48:57 +0800

    LT27: 移除数组中给的值的元素

---
+++ src/array/lt27_test.go
@@ -0,0 +1,27 @@
+// 移除数组中给的值的元素
+package array
+
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	var bound int
+
+	for i, v := range nums {
+		if v == val {
+			continue
+		}
+
+		swap(nums, i, bound)
+		bound++
+	}
+
+	return bound
+}
+
+func swap(nums []int, i, j int) {
+	tmp := nums[i]
+	nums[i] = nums[j]
+	nums[j] = tmp
+}
