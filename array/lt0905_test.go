0368dac91797e1365fc7bc0134ab5b4df6bef5eb
zhaoyun.248 <zhaoyun.248@bytedance.com>
2021-07-05 15:01:33 +0800

    LT905: 把数组汇总偶数放前面，奇数放后面

---
+++ array/lt0905_test.go
@@ -0,0 +1,18 @@
+// 把数组汇总偶数放前面，奇数放后面
+package array
+
+func sortArrayByParity(nums []int) []int {
+
+	for i, j := 0, len(nums)-1; i < j; {
+		for ; i < j && nums[i]%2 == 0; i++ {
+		}
+		for ; i < j && nums[j]%2 != 0; j-- {
+		}
+
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+
+	return nums
+}
