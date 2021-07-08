b74065acd18039ea82cff954ae3057c4b479da24
zhaoyun.248 <zhaoyun.248@bytedance.com>
2021-07-08 10:52:53 +0800

    LT142: 找出链表中环的起点位置 solution2

--- list/lt0142_test.go
+++ list/lt0142_test.go
@@ -14,3 +14,21 @@ func detectCycle(head *ListNode) *ListNode {
 
 	return nil
 }
+
+func detectCycle2(head *ListNode) *ListNode {
+	for slow, fast := head, head; fast != nil && fast.Next != nil; {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			p1, p2 := slow, head
+			for p1 != p2 {
+				p1 = p1.Next
+				p2 = p2.Next
+			}
+			return p1
+		}
+	}
+
+	return nil
+}
