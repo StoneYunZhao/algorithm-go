b519960a4736f45dbfd50cc0153dacc24bf03b08
zhaoyun.248 <zhaoyun.248@bytedance.com>
2021-07-08 15:19:26 +0800

    LT19: 移除链表中倒数第 n 个元素 solution2

--- list/lt0019_test.go
+++ list/lt0019_test.go
@@ -22,3 +22,24 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 
 	return head
 }
+
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Next: head}
+
+	slow, fast := dummy, head
+
+	for fast != nil  {
+		if n > 0 {
+			n--
+			fast = fast.Next
+			continue
+		}
+
+		fast = fast.Next
+		slow = slow.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return dummy.Next
+}
