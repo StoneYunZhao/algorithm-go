e0169feacf729e859e371836b64964b5e25326e3
zhaoyun.248 <zhaoyun.248@bytedance.com>
2021-07-13 20:22:00 +0800

    LT21: 合并两个有序链表 solution2

--- list/lt0021_test.go
+++ list/lt0021_test.go
@@ -46,10 +46,10 @@ func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 
 	if l1.Val < l2.Val {
-		l1.Next = mergeTwoLists(l1.Next, l2)
+		l1.Next = mergeTwoLists2(l1.Next, l2)
 		return l1
 	}
 
-	l2.Next = mergeTwoLists(l1, l2.Next)
+	l2.Next = mergeTwoLists2(l1, l2.Next)
 	return l2
 }
